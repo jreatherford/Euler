747de5e0fb9e76c6158697d8ec58e761a5308ae1
James Reatherford <jamesreatherford@gmail.com>
2013-11-08 01:38:38 -0600

    updated 441.  Don't have it yet.  Can't generate enough coprimes...

--- 441/441.py
+++ 441/441.py
@@ -1,30 +1,23 @@
-import sys
-
 def genCoprimePairs (max):
-    pairs = coprimeHelp (2,1, max)
-    return sorted(pairs)
-    
-def coprimeHelp (m, n, max):
-    pairs = [(m,n)]
-    
-    if (m <= max): 
-        pairs.extend(coprimeHelp ((2*m - n), m, max))
-        pairs.extend(coprimeHelp ((2*m + n), m, max))
-        pairs.extend(coprimeHelp ((m + 2*n), n, max))
-    
-    return pairs
+    coPrimes = [(2,1)]
+    i = 0
+    while i < len(coPrimes):
+	    m = coPrimes[i][0]
+	    n = coPrimes[i][1]
 
+	    if ((2*m - n) <= max): 
+	    	coPrimes.append(((2*m - n), m))
+	    if ((2*m + n) <= max): 
+	    	coPrimes.append(((2*m + n), m))
+	    if ((m + 2*n) <= max): 
+	    	coPrimes.append(((m + 2*n), n))
+	    i += 1   
+    return coPrimes
 
 size = pow(10,4)
 coprimes = genCoprimePairs(size)
 print len(coprimes)
 print "done"
-#R_set = {}
-
-#for pair in coprimes:
-#    R_set[pair[0]] = 1.0/(pair[0]*pair[1])
-    
-#print R_set
     
 
 
